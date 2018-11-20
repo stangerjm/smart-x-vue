b1dbdb80972c9bb4704fff620342aef0dc19ed8a
stangerjm <jamesstanger987@gmail.com>
2018-11-20 11:43:28 -0800

    Added smart-accordion unit test

---
+++ tests/unit/componentTests/smart-card.spec.js
@@ -0,0 +1,8 @@
+import { createComponentGenerator } from '../helpers';
+import SmartCard from '../../../src/components/smart-card.vue';
+
+const mountSmartCard = createComponentGenerator(SmartCard);
+
+describe('smart-card.vue', () => {
+  it('', () => {});
+});
