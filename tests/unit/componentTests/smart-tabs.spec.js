2a55a25654ef8580d874264ec493820647fd4ed4
stangerjm <jamesstanger987@gmail.com>
2018-12-14 12:27:31 -0800

    Added required marking on bit-select

---
+++ tests/unit/componentTests/smart-tabs.spec.js
@@ -0,0 +1,8 @@
+import { createComponentGenerator } from '../helpers';
+import SmartTabs from '../../../src/components/smart-tabs.vue';
+
+const mountSmartTabs = createComponentGenerator(SmartTabs);
+
+describe('smart-tabs.vue', () => {
+  it('', () => {});
+});
