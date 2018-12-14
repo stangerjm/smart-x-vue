cfc13f7cdb1bf127bdac9d6c9c00892beb361f45
stangerjm <jamesstanger987@gmail.com>
2018-12-14 14:17:32 -0800

    Added default, shared slot in smart-tabs

---
+++ tests/unit/componentTests/block-messages.spec.js
@@ -0,0 +1,8 @@
+import { createComponentGenerator } from '../helpers';
+import BlockMessages from '../../../src/components/block-messages.vue';
+
+const mountBlockMessages = createComponentGenerator(BlockMessages);
+
+describe('block-messages.vue', () => {
+  it('', () => {});
+});
