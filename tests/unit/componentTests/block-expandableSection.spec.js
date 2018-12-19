4085b048692901bc129ddc56fd0ade4e43da4a3f
stangerjm <jamesstanger987@gmail.com>
2018-12-18 16:30:29 -0800

    Added block-expandableSection unit test

--- tests/unit/componentTests/block-expandableSection.spec.js
+++ tests/unit/componentTests/block-expandableSection.spec.js
@@ -1,13 +1,33 @@
-// import { createComponentGenerator } from '../helpers';
-// import BlockExpandableSection from '../../../src/components/block-expandableSection.vue';
-//
-// const mountBlockExpandableSection = createComponentGenerator(BlockExpandableSection, {
-//   attachToDocument: true,
-//   slots: {
-//     default: '<h1 style="height: 100px;">Hello there</h1>',
-//   },
-// });
+import { createComponentGenerator } from '../helpers';
+import BlockExpandableSection from '../../../src/components/block-expandableSection.vue';
+
+const content = '<h1 class="test-content">Hello there</h1>';
+
+const mountBlockExpandableSection = createComponentGenerator(BlockExpandableSection, {
+  attachToDocument: true,
+  slots: {
+    default: content,
+  },
+});
+
+const blockExpandableSection = mountBlockExpandableSection({
+  isExpanded: true,
+});
 
 describe('block-expandableSection.vue', () => {
-  it('sizes the container to the exact height of the inner content', () => {});
+  it('allows content to be passed in', () => {
+    const testContent = blockExpandableSection.find('.test-content');
+
+    expect(testContent.html()).toEqual(content);
+  });
+
+  it('allows expanded state to be set from the outside', () => {
+    expect(blockExpandableSection.props('isExpanded')).toEqual(true);
+
+    blockExpandableSection.setProps({ isExpanded: false });
+    expect(blockExpandableSection.props('isExpanded')).toEqual(false);
+
+    blockExpandableSection.setProps({ isExpanded: true });
+    expect(blockExpandableSection.props('isExpanded')).toEqual(true);
+  });
 });
