7bd30f10a305db2158292ee553f50fa54c9a2dc8
stangerjm <jamesstanger987@gmail.com>
2019-04-04 13:52:48 -0700

    Finished smart-modal prototype and included a unit test for the final product.

---
+++ src/components/smart-modal.vue
@@ -0,0 +1,150 @@
+<template>
+
+  <!-- Modal wrapper -->
+  <section class="smart-modal"
+           role="dialog"
+           v-if="value">
+
+    <!-- Container for all modal content -->
+    <div class="smart-modal--content"
+         :class="{ 'smart-modal--centeredContent': centerModal }">
+
+      <!-- Modal header -->
+      <section class="smart-modal--head">
+
+        <!-- Close button -->
+        <button class="smart-modal--close" @click="closeModal">CLOSE</button>
+
+        <!-- Modal title -->
+        <h2 class="smart-modal--title" v-if="modalTitle">{{modalTitle}}</h2>
+      </section>
+
+      <!-- Modal body -->
+      <section class="smart-modal--body">
+
+        <!-- Slot for content -->
+        <slot></slot>
+
+      </section>
+
+    </div>
+
+  </section>
+
+</template>
+
+<script>
+export default {
+  name: 'smart-modal',
+  props: {
+    modalTitle: {
+      type: String,
+    },
+    value: {
+      type: Boolean,
+      required: true,
+    },
+    centerModal: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      focusableEls: null,
+      firstFocusableEl: null,
+      lastFocusableEl: null,
+      focusedElBeforeOpen: null,
+    };
+  },
+  watch: {
+    value(isOpen) {
+      if (isOpen) {
+        this.$nextTick(() => {
+          this.onOpen();
+        });
+        return;
+      }
+
+      this.onClose();
+    },
+  },
+  methods: {
+    onOpen() {
+      if (this.$el.querySelectorAll == null) {
+        return;
+      }
+
+      const focusableEls = this.$el
+        .querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]');
+
+      this.focusableEls = [...focusableEls];
+
+      const [firstFocusableEl] = this.focusableEls;
+      const [lastFocusableEl] = this.focusableEls.slice(-1);
+
+      this.firstFocusableEl = firstFocusableEl;
+      this.lastFocusableEl = lastFocusableEl;
+
+      this.focusedElBeforeOpen = document.activeElement;
+
+      this.firstFocusableEl.focus();
+
+      this.$el.addEventListener('keydown', (e) => {
+        this.handleKeyDown(e);
+      });
+    },
+    onClose() {
+      if (this.focusedElBeforeOpen) {
+        this.focusedElBeforeOpen.focus();
+      }
+    },
+    closeModal() {
+      this.$emit('input', !this.value);
+    },
+    handleKeyDown(e) {
+      const Dialog = this;
+      const KEY_TAB = 9;
+      const KEY_ESC = 27;
+
+      function handleBackwardTab() {
+        if (document.activeElement === Dialog.firstFocusableEl) {
+          e.preventDefault();
+          Dialog.lastFocusableEl.focus();
+        }
+      }
+
+      function handleForwardTab() {
+        if (document.activeElement === Dialog.lastFocusableEl) {
+          e.preventDefault();
+          Dialog.firstFocusableEl.focus();
+        }
+      }
+
+      switch (e.keyCode) {
+        case KEY_TAB:
+          if (Dialog.focusableEls.length === 1) {
+            e.preventDefault();
+            break;
+          }
+          if (e.shiftKey) {
+            handleBackwardTab();
+          } else {
+            handleForwardTab();
+          }
+          break;
+
+        case KEY_ESC:
+          Dialog.closeModal();
+          break;
+        default:
+          break;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped lang='scss'>
+@import '../../styles/sass/components/smart/modal/smart-modal';
+</style>
---
+++ tests/unit/componentTests/smart-modal.spec.js
@@ -0,0 +1,78 @@
+import { mount } from '@vue/test-utils';
+
+import { Click } from '../helpers';
+import SmartModal from '../../../src/components/prototypes/smart-modal-PROTO.vue';
+
+const modalWrapper = mount({
+  components: {
+    SmartModal,
+  },
+  template: `
+    <div class="modal-test">
+      <smart-modal modal-title="test" v-model="isOpen">
+        <h1 class="modal-test--content">Hello</h1>
+
+        <button class="modal-test--btn">Test</button>
+      </smart-modal>
+
+      <button class="modal-test--open" @click="openModal">Open</button>
+    </div>
+  `,
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.isOpen = true;
+    },
+  },
+}, {
+  attachToDocument: true,
+});
+
+function openModal(smartModal) {
+  const openModalBtn = smartModal.find('.modal-test--open');
+
+  Click(openModalBtn);
+}
+
+describe('smart-modal.vue', () => {
+  it('allows open state to be managed internally and externally', () => {
+    // Modal can be opened from the outside
+    const openModalBtn = modalWrapper.find('.modal-test--open');
+
+    Click(openModalBtn);
+
+    const modal = modalWrapper.find('.smart-modal');
+
+    expect(modal.exists()).toEqual(true);
+
+    // Modal can be closed from the inside, and the outside value updates
+    const closeModalBtn = modalWrapper.find('.smart-modal--close');
+
+    Click(closeModalBtn);
+
+    const closedModal = modalWrapper.find('.smart-modal');
+
+    expect(closedModal.exists()).toEqual(false);
+    expect(modalWrapper.vm.isOpen).toEqual(false);
+  });
+
+  it('allows content to be passed in', () => {
+    openModal(modalWrapper);
+
+    const content = modalWrapper.find('.modal-test--content');
+
+    expect(content.exists()).toEqual(true);
+  });
+
+  it('sets focus to the first focusable element in the modal', () => {
+    openModal(modalWrapper);
+
+    const closeBtn = modalWrapper.find('.smart-modal--close');
+
+    expect(document.activeElement).toEqual(closeBtn.element);
+  });
+});
